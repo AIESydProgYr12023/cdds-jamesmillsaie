5e1915e71ff5790ecb992d365d83158469a1fe46
James Mills <james.mills@aie.edu.au>
2023-06-01 10:38:24 +1000

    Console Binary Tree tutorial

--- Tutorials/BinaryTree_Console/main.cpp
+++ Tutorials/BinaryTree_Console/main.cpp
@@ -17,20 +17,66 @@ struct Node
 
 };
 
-void PreOrderPrint(Node* _n)
+void PreOrderPrint(Node* _n, const char* _dir)
 {
-    // TODO 
+    if (_n != nullptr)
+    {
+        std::cout << _dir << ": " << _n->value << std::endl;
+        PreOrderPrint(_n->left, "Left");
+        PreOrderPrint(_n->right, "Right");
+    }
 }
 
-void PostOrderPrint(Node* _n)
+void PostOrderPrint(Node* _n, const char* _dir)
 {
-    // TODO 
+    if (_n != nullptr)
+    {
+        PostOrderPrint(_n->left, "Left");
+        PostOrderPrint(_n->right, "Right");
+        std::cout << _dir << ": " << _n->value << std::endl;
+    }
+}
+
+void InOrderPrint(Node* _n, const char* _dir)
+{
+    if (_n != nullptr)
+    {
+        InOrderPrint(_n->left, "Left");
+        std::cout << _dir << ": " << _n->value << std::endl;
+        InOrderPrint(_n->right, "Right");
+    }
 }
 
 Node* Find(Node* _n, int _value)
 {
-    // TODO
-    return nullptr;
+    Node* current = _n;
+    Node* parent = nullptr;
+
+    while (current != nullptr && current->value != _value)
+    {
+        // Update parent to current
+        parent = current;
+
+        if (_value < current->value)
+            current = current->left;
+        else
+            current = current->right;
+    }
+
+    return current;
+}
+
+void Find(Node* _n, int _value, Node*& _parent)
+{
+    while (_n != nullptr && _n->value != _value)
+    {
+        _parent = _n;
+
+        if (_value < _n->value)
+            _n = _n->left;
+        else
+            _n = _n->right;
+    }
 }
 
 void Insert(Node* _root, Node* _nodeToInsert)
@@ -67,6 +113,13 @@ int main()
 
     // TODO
     // Test the methods above
+
+    PreOrderPrint(&root, "root");
+    std::cout << std::endl;
+    PostOrderPrint(&root, "root");
+    std::cout << std::endl;
+    InOrderPrint(&root, "root");
+    std::cout << "---------------" << std::endl;
 
     return 0;
 }
